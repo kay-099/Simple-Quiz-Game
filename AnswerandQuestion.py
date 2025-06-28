e0147725f194b0a7eaeeeac08428408eadcbdf1e
kay-099 <mwanzakay6@gmail.com>
2025-06-28 22:34:56 +0200

    simple quiz game

---
+++ AnswerandQuestion.py
@@ -0,0 +1,2 @@
+Questionlist = ["what color is a banana", "what is the actual color of the sun", "When does water turn to ice", "what is RGB"]
+Answerlist = ["Yellow", "White", "When it frezzes", "Red Green Blue"]
---
+++ Marker.py
@@ -0,0 +1,24 @@
+import AnswerandQuestion
+
+# the below variables are used to keep track of question and answers
+score = 0
+totalQestionList = len(AnswerandQuestion.Questionlist)
+
+def Marker(index):
+    answers = AnswerandQuestion.Answerlist[index]
+    print("Answer:..")
+    userinput = input()
+    
+    # return the score if correct.
+    if userinput.lower() == answers.lower():
+        return 1
+    else :
+        return 0
+    
+
+# iterates over the question and calls the marker function for the answers
+for i in AnswerandQuestion.Questionlist:
+    print(i)
+    score += Marker(score)
+
+print(f"\n{score} out of {totalQestionList} is your mark")
